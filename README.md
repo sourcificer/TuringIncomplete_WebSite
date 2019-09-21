13c39cf7410d1693d74fc90df92e592a76e693b5
drkfyre <thedrkfyre@gmail.com>
2019-09-21 18:59:54 +0530

    Removed contact_us form

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # TuringIncomplete_WebSite
 TuringIncomplete's WebSite source code
+
+**To run:**
+
+```
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py runserver
+```
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,6 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView
-from django.http import HttpResponse, HttpResponseRedirect
-from .forms import ClientContactForm
-from django.core.mail import send_mail, BadHeaderError
 
 # Create your views here.
 def home_page(request):
 
-    if request.method == 'GET':
-        form = ClientContactForm()
-    else:
-        form = ClientContactForm(request.POST)
-        if form.is_valid():
-            subject = form.cleaned_data['subject']
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-            try:
-                send_mail(subject,message,email,['tenssyonben@gmail.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid Header Found')
-            return redirect('home_page')
-
-    return render(request,'index.html',{'form':form})
+    return render(request,'index.html')

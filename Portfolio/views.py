fa44ece4344cdf1fa02602e704b5d566143e6247
iamharpar <iamharpar1@gmail.com>
2019-09-24 14:02:40 +0530

    Added location Based Currency

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.gis.geoip2 import GeoIP2
+import json
+from urllib.request import urlopen
 # SendGrid API libraries
 import os
 from sendgrid import SendGridAPIClient
@@ -9,14 +12,27 @@ from sendgrid.helpers.mail import Mail
 class Portfolio(View):
     template_name = 'index.html'
 
+    def locationChecker(self, request):
+        url = 'http://ipinfo.io/json'
+        response = urlopen(url)
+        data = json.load(response)
+        country = data['country']
+        return country
+
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        context = {
+            'country': self.locationChecker(request)
+        }
+        return render(request, self.template_name, {'context':context})
 
     def post(self, request, *args, **kwargs):
         subject = request.POST.get('subject')
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
+        context = {
+            'country': self.locationChecker(request)
+        }
 
         try:
             mail_message = Mail(
@@ -28,8 +44,7 @@ class Portfolio(View):
             sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
             response = sg.send(mail_message)
             print(response.headers)
-            return redirect('home_page')
         except Exception as e:
             print(str(e))
 
-        return render(request, self.template_name)
+        return render(request, self.template_name,{'context':context})

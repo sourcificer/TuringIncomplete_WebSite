f6c7cca2fe8c670eef57fd46cd3bb82114821013
shubham singh <shubham1.singh15@gmail.com>
2019-10-01 19:40:55 +0530

    Fixed JSON mechanism (#14)
    * fixed e.message to str(e)
    
    * Added pricing plans
    
    * AJAX mechanism working

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import Portfolio
+from .views import Portfolio, send_message
 
 urlpatterns = [
     path('', Portfolio.as_view(), name='index'),
+    path('send-message/', send_message, name="send_message"),
+
 ]

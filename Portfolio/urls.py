36f733f765a34b6762005b35f28732d38b86a844
Shubham singh <shubham1.singh15@gmail.com>
2019-10-09 22:44:56 +0530

    Changed send_message to CBV  SendMessageAjaxView

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from .views import Portfolio, send_message
+from .views import Portfolio,  SendMessageAjaxView
 
 urlpatterns = [
     path('', Portfolio.as_view(), name='index'),
-    path('send-message/', send_message, name="send_message"),
+    path('send-message/',  SendMessageAjaxView.as_view(), name="send_message"),
 
 ]

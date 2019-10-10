a02a1f97eb87ef65e187f6a00d5dc4efe4d7a2e1
Shubham singh <shubham1.singh15@gmail.com>
2019-10-10 06:17:11 +0530

    added a CartView with basic template

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
-from .views import Portfolio,  SendMessageAjaxView
+from .views import (
+    Portfolio, CartView,
+    SendMessageAjaxView,
+
+)
 
 urlpatterns = [
     path('', Portfolio.as_view(), name='index'),
+    path('view-cart/', CartView.as_view(), name="cart_view"),
     path('send-message/',  SendMessageAjaxView.as_view(), name="send_message"),
 
 ]
